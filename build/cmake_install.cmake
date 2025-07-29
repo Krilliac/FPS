92d195dddaf36a570cfae47f3d95fe2f57cd491e
Krilliac <natew94@gmail.com>
2025-07-29 04:49:25 -0500

    update

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -34,6 +34,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("D:/Game/FPSGame/build/curl/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("D:/Game/FPSGame/build/angelscript/cmake_install.cmake")
 endif()
 
